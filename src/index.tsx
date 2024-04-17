000453a672fc3447e61ea9bc835afff7f2ea5205
Bogdan Samonov <BVSamonov@gmail.com>
2024-04-17 10:09:57 +0300

    fix: fixing

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,10 @@
-import { createRoot } from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import App from './components/app';
+import './index.css';
 
-const container = document.getElementById('root') as HTMLElement;
-const root = createRoot(container);
-
-root.render(<App tab="home" />);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+);
